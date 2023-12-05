0b120272b429d1dbefcf102532535ba312ad4ad1
Kaline Dutra <kalineduttra@gmail.com>
2023-12-04 22:57:25 -0300

    adicionandoo componentes

---
+++ src/br/com/imposto/view/TelaPis.java
@@ -0,0 +1,34 @@
+package br.com.imposto.view;
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextField;
+
+public class TelaPis implements TelaImposto {
+
+	private TextField txtValor;
+	private Button btnCalcular;
+	
+	public TelaPis() {
+		Frame frame = new Frame ("Calculo Imposto Pis");
+		frame.add("North", new Label("Valor para calcular"));
+		txtValor = new TextField();
+		frame.add("Center", txtValor);
+		
+		Panel panel = new Panel();
+		btnCalcular = new Button("Calcular");
+		frame.add("South", btnCalcular);
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	@Override
+	public float getValor() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
